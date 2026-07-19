905d58da1c87438306badb3730d3430e860b730b
Plecra <marli@frost.red>
2026-07-19 20:06:33 +0100

    Implement type variables with a new Universe judgement.
    We also extend the construction of the context index of the derivation check to ensure it doesnt introduce any type variables implicitly.

--- derivations/src/lib.rs
+++ derivations/src/lib.rs
@@ -1,11 +1,40 @@
 use ast::TypedEv;
+use kernel::{Typed, Proof, Universe};
 
 use std::collections::HashMap;
 #[derive(Debug)]
-pub struct ContextIndex(HashMap<String, core::Type>);
+pub struct ContextIndex(HashMap<String, Option<Proof<Universe>>>);
+
+fn verify_universe(t: &core::Type) -> kernel::Proof<kernel::Universe> {
+    match t {
+        core::Type::Var(s) => kernel::Proof::type_var(s.clone()),
+        core::Type::Arrow(a, b) => {
+            kernel::Proof::arrow(verify_universe(a), verify_universe(b))
+        }
+    }
+}
 impl ContextIndex {
-    pub fn new(binds: impl Iterator<Item = (String, core::Type)>) -> Self {
-        ContextIndex(binds.collect())
+    fn consistent(&self, cx: &[String]) -> bool {
+        cx.iter().all(|s| self.0.contains_key(s))
+    }
+    fn verify_ty(&self, ty: &core::Type) -> Result<Proof<Universe>, Error> {
+        let univ = verify_universe(ty);
+        if !self.consistent(&univ.get().context) {
+            return Err(Error(()));
+        }
+        Ok(univ)
+    }
+    pub fn new(binds: impl Iterator<Item = (String, kernel::Bound)>) -> Result<Self, Error> {
+        let mut this = ContextIndex(HashMap::new());
+        for (s, b) in binds {
+            this.0.insert(s, match b {
+                kernel::Bound::TypeVar => None,
+                kernel::Bound::Var(ty) => {
+                    Some(this.verify_ty(&ty)?)
+                },
+            });
+        }
+        Ok(this)
     }
 }
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -16,7 +45,7 @@ impl From<kernel::IncompatibleStep> for Error {
     }
 }
 /// [`kernel::Proof::app`] with automatic merging of the contexts of the function and argument.
-pub fn app(f: kernel::Proof, a: kernel::Proof) -> Result<kernel::Proof, Error> {
+pub fn app(f: Proof<Typed>, a: Proof<Typed>) -> Result<Proof<Typed>, Error> {
     let mut cx = vec![];
     let mut present = std::collections::HashSet::new();
     for (s, t) in f.get().context.iter().chain(&a.get().context) {
@@ -27,7 +56,7 @@ pub fn app(f: kernel::Proof, a: kernel::Proof) -> Result<kernel::Proof, Error> {
     Ok(f.structural(cx.clone())?.app(a.structural(cx)?)?)
 }
 /// [`kernel::Proof::abs`] with automatic exchange of the variable binding anywhere in the context.
-pub fn abs(body: kernel::Proof, param: String, ty: core::Type) -> Result<kernel::Proof, Error> {
+pub fn abs(body: Proof<Typed>, param: String, ty: core::Type) -> Result<Proof<Typed>, Error> {
     let mut cx = body.get().context.clone();
     let n = cx.len() - 1;
 
@@ -38,12 +67,12 @@ pub fn abs(body: kernel::Proof, param: String, ty: core::Type) -> Result<kernel:
         .enumerate()
         .find(|(_, (n, _))| n == &param)
     {
-        if t != &ty {
+        if t != &kernel::Bound::Var(ty) {
             return Err(Error(()));
         }
         cx.swap(i, n);
     } else {
-        cx.push((param, ty));
+        cx.push((param, kernel::Bound::Var(ty)));
     }
     Ok(body.structural(cx)?.abs()?)
 }
@@ -55,12 +84,13 @@ pub fn abs(body: kernel::Proof, param: String, ty: core::Type) -> Result<kernel:
 /// and store `HasType(derivation, ty)`.
 ///
 /// The `cx` will be unchanged after this function returns.
-pub fn verify(ev: &ast::TypedEv, cx: &mut ContextIndex) -> Result<kernel::Proof, Error> {
+pub fn verify(ev: &ast::TypedEv, cx: &mut ContextIndex) -> Result<Proof<Typed>, Error> {
     match ev {
         TypedEv::Var(n) => {
             cx.0.get(n)
-                .map(|t| kernel::Proof::var(n.clone(), t.clone()))
+                .and_then(|t| t.as_ref())
                 .ok_or(Error(()))
+                .and_then(|t| Ok(kernel::Proof::var(n.clone(), t.clone())?))
         }
         TypedEv::App(f, a) => {
             let ft = verify(f, cx)?;
@@ -68,7 +98,7 @@ pub fn verify(ev: &ast::TypedEv, cx: &mut ContextIndex) -> Result<kernel::Proof,
             app(ft, at)
         }
         TypedEv::Abs(s, ty, b) => {
-            let old = cx.0.insert(s.clone(), ty.clone());
+            let old = cx.0.insert(s.clone(), Some(cx.verify_ty(ty)?));
             let bt = verify(b, cx)?;
             if let Some(old) = old {
                 cx.0.insert(s.clone(), old);
@@ -80,25 +110,25 @@ pub fn verify(ev: &ast::TypedEv, cx: &mut ContextIndex) -> Result<kernel::Proof,
     }
 }
 /// A convenient way to construct a derivation while verifying it.
-pub struct RecordedProof(TypedEv, kernel::Proof);
+pub struct RecordedProof(TypedEv, kernel::Proof<Typed>);
 impl RecordedProof {
     pub fn evidence(&self) -> &TypedEv {
         &self.0
     }
-    pub fn proof(&self) -> &kernel::Proof {
+    pub fn proof(&self) -> &kernel::Proof<Typed> {
         &self.1
     }
-    pub fn into_inner(self) -> (TypedEv, kernel::Proof) {
+    pub fn into_inner(self) -> (TypedEv, kernel::Proof<Typed>) {
         (self.0, self.1)
     }
     pub fn show(ev: TypedEv, cx: &mut ContextIndex) -> Result<Self, Error> {
         let proof = verify(&ev, cx)?;
         Ok(RecordedProof(ev, proof))
     }
-    pub fn var(name: String, ty: core::Type) -> Self {
+    pub fn var(name: String, ty: &core::Type) -> Result<Self, Error> {
         let ev = TypedEv::Var(name.clone());
-        let proof = kernel::Proof::var(name, ty);
-        RecordedProof(ev, proof)
+        let proof = kernel::Proof::var(name, verify_universe(ty))?;
+        Ok(RecordedProof(ev, proof))
     }
     pub fn app(self, arg: RecordedProof) -> Result<Self, Error> {
         let ev = TypedEv::App(Box::new(self.0), Box::new(arg.0));
@@ -118,7 +148,7 @@ impl RecordedProof {
     pub fn consistent(&self) -> Result<(), Error> {
         (verify(
             &self.0,
-            &mut ContextIndex::new(self.1.get().context.iter().cloned()),
+            &mut ContextIndex::new(self.1.get().context.iter().cloned())?,
         )?
         .get()
             == self.1.get())
