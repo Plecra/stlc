c6fbb8864f2f2104e6dc4509fbc73434dcf64b46
Plecra <marli@frost.red>
2026-07-19 20:06:33 +0100

    Implement type variables with a new Universe judgement.
    We also extend the construction of the context index of the derivation check to ensure it doesnt introduce any type variables implicitly.

--- demo/tests/examples.rs
+++ demo/tests/examples.rs
@@ -1,23 +1,23 @@
 #[test]
 fn examples() {
     let (a, errors) = parser::parse(&mut &b"\\x : A. x"[..]);
-    let p = derivations::verify(&a, &mut derivations::ContextIndex::new([].into_iter())).unwrap();
+    let p = derivations::verify(&a, &mut derivations::ContextIndex::new([("A".into(), kernel::Bound::TypeVar)].into_iter()).unwrap()).unwrap();
     assert!(errors.is_empty());
     assert_eq!(format!("{a:?}"), r#"Abs("x", Var("A"), Var("x"))"#);
-    assert_eq!(format!("{p:?}"), r#"Proof(Typed { context: [], expr: Abs("x", Var("x")), ty: Arrow(Var("A"), Var("A")) })"#);
+    assert_eq!(format!("{p:?}"), r#"Proof(Typed { context: [("A", TypeVar)], expr: Abs("x", Var("x")), ty: Arrow(Var("A"), Var("A")) })"#);
     let (a, errors) = parser::parse(&mut &b"
         \\toString : Int -> String. \\add : Int -> Int -> Int. \\n : Int. \\m : Int. toString (add n m)
     "[..]);
-    let p = derivations::verify(&a, &mut derivations::ContextIndex::new([].into_iter())).unwrap();
+    let p = derivations::verify(&a, &mut derivations::ContextIndex::new([("Int".into(), kernel::Bound::TypeVar), ("String".into(), kernel::Bound::TypeVar)].into_iter()).unwrap()).unwrap();
     assert!(errors.is_empty());
     assert_eq!(format!("{a:?}"), r#"Abs("toString", Arrow(Var("Int"), Var("String")), Abs("add", Arrow(Var("Int"), Arrow(Var("Int"), Var("Int"))), Abs("n", Var("Int"), Abs("m", Var("Int"), App(Var("toString"), App(App(Var("add"), Var("n")), Var("m")))))))"#);
     assert_eq!(format!("{p:?}"),
-        concat!("Proof(Typed { context: [], ",
+        concat!("Proof(Typed { context: [(\"Int\", TypeVar), (\"String\", TypeVar)], ",
             r#"expr: Abs("toString", Abs("add", Abs("n", Abs("m", App(Var("toString"), App(App(Var("add"), Var("n")), Var("m"))))))), "#,
             r#"ty: Arrow(Arrow(Var("Int"), Var("String")), Arrow(Arrow(Var("Int"), Arrow(Var("Int"), Var("Int"))), Arrow(Var("Int"), Arrow(Var("Int"), Var("String"))))) "#,
         "})"));
     let (a, errors) = parser::parse(&mut &b"\\x : A. \\y : B. x y"[..]);
-    let _ = derivations::verify(&a, &mut derivations::ContextIndex::new([].into_iter())).unwrap_err();
+    let _ = derivations::verify(&a, &mut derivations::ContextIndex::new([("A".into(), kernel::Bound::TypeVar), ("B".into(), kernel::Bound::TypeVar)].into_iter()).unwrap()).unwrap_err();
     assert!(errors.is_empty());
     assert_eq!(format!("{a:?}"), r#"Abs("x", Var("A"), Abs("y", Var("B"), App(Var("x"), Var("y"))))"#);
 }
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,5 +1,11 @@
 use core::{Expr, Type};
-pub type Context = Vec<(String, Type)>;
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Bound {
+    Var(Type),
+    TypeVar,
+}
+pub type Context = Vec<(String, Bound)>;
+pub type TypeContext = Vec<String>;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Typed {
     pub context: Context,
@@ -9,6 +15,11 @@ pub struct Typed {
 #[allow(non_snake_case)]
 fn Typed(context: Context, expr: Expr, ty: Type) -> Typed {
     Typed { context, expr, ty }
+}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Universe {
+    pub context: TypeContext,
+    pub ty: Type,
 }
 /// A proof of a [`Typed`] judgement, built from the inference rules defined
 /// on this type.
@@ -40,25 +51,30 @@ fn Typed(context: Context, expr: Expr, ty: Type) -> Typed {
 /// rule's parameters so we avoid redundantly passing them as well. (For these STLC rules, app and abs'
 /// premises both entirely fix their parameters)
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Proof(Typed);
+pub struct Proof<T>(T);
 
-impl Proof {
-    pub fn get(&self) -> &Typed {
+impl<T> Proof<T> {
+    pub fn get(&self) -> &T {
         &self.0
     }
-    pub fn into_inner(self) -> Typed {
+    pub fn into_inner(self) -> T {
         self.0
     }
 }
 
 #[derive(Debug)]
 pub struct IncompatibleStep(());
-impl Proof {
+impl Proof<Typed> {
     /// ```ignore
     /// x : A |- x : A
     /// ```
-    pub fn var(name: String, ty: Type) -> Self {
-        Proof(Typed(vec![(name.clone(), ty.clone())], Expr::Var(name), ty))
+    pub fn var(name: String, ty: Proof<Universe>) -> Result<Self, IncompatibleStep> {
+        let mut context = ty.0.context.into_iter().map(|s| (s, Bound::TypeVar)).collect::<Vec<_>>();
+        if context.iter().any(|(tyname, _)| tyname == &name) {
+            return Err(IncompatibleStep(()));
+        }
+        context.push((name.clone(), Bound::Var(ty.0.ty.clone())));
+        Ok(Proof(Typed(context, Expr::Var(name), ty.0.ty)))
     }
     /// ```ignore
     /// cx |- f : A -> B
@@ -66,7 +82,7 @@ impl Proof {
     /// ---
     /// cx |- f e : B
     /// ```
-    pub fn app(self, arg: Proof) -> Result<Self, IncompatibleStep> {
+    pub fn app(self, arg: Proof<Typed>) -> Result<Self, IncompatibleStep> {
         let (context, b_ty, f, e) = match self.0.ty {
             Type::Arrow(argt, rett) if (&arg.0.context, &arg.0.ty) == (&self.0.context, &*argt) => {
                 (arg.0.context, *rett, self.0.expr, arg.0.expr)
@@ -89,7 +105,10 @@ impl Proof {
         Ok(Proof(Typed(
             self.0.context,
             Expr::Abs(param, Box::new(self.0.expr)),
-            Type::Arrow(Box::new(t.clone()), Box::new(self.0.ty.clone())),
+            Type::Arrow(Box::new(match t {
+                Bound::Var(ty) => ty,
+                _ => return Err(IncompatibleStep(())),
+            }), Box::new(self.0.ty)),
         )))
     }
     /// We capture the weakening and contraction rules in a single verification step:
@@ -107,7 +126,7 @@ impl Proof {
     /// ---
     /// cx, y : U, x : T, cx2 |- e : A
     /// ```
-    pub fn structural(self, context: Vec<(String, Type)>) -> Result<Self, IncompatibleStep> {
+    pub fn structural(self, context: Vec<(String, Bound)>) -> Result<Self, IncompatibleStep> {
         use std::collections::HashMap;
         // note: this same logic can be implemented 'syntactically' by
         //     forall i in context, forall j, i < j ==> context[i].name != context[j].name
@@ -121,10 +140,29 @@ impl Proof {
         // Check that the context is well formed. That just means that the names are unique here.
         if index.len() != context.len()
         // And require that all the bindings in the proof's context are identically mapped.
-        || !self.get().context.iter().all(|(s, t)| index.get(&**s) == Some(&t))
+        || !self.get().context.iter().all(|(s, b)| index.get(&**s) == Some(&b))
         {
             return Err(IncompatibleStep(()));
         }
         Ok(Proof(Typed(context, self.0.expr, self.0.ty)))
+    }
+}
+
+/// In this basic form, this is a little silly: *all* types are in the universe and we're just
+/// collecting the free variables.
+impl Proof<Universe> {
+    pub fn type_var(name: String) -> Self {
+        Proof(Universe {
+            context: vec![name.clone()],
+            ty: Type::Var(name),
+        })
+    }
+    pub fn arrow(self, arg: Self) -> Self {
+        let mut context = self.0.context;
+        context.extend(arg.0.context);
+        Proof(Universe {
+            context,
+            ty: Type::Arrow(Box::new(self.0.ty), Box::new(arg.0.ty)),
+        })
     }
 }
