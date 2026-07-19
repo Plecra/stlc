10cb8ac8bd281d93fa8569e481b9bfc9763a44ec
Plecra <marli@frost.red>
2026-07-19 16:57:24 +0100

    docs: Some details on literals

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,13 @@
-/// The interface of the Core language, defining the terms and types that the kernel checks
+//! The interface of the Core language, defining the terms and types that the kernel checks
 
+/// 
+/// ## Literals
+/// 
+/// String and numerics literals are logically the same as extending the context with
+/// families of variables typed with the literal's type.
+/// `String : Type, "hello" : String |- "hello" : String`
+/// `derivations::verify` can handle this by adding type expressions for String and Int
+/// literals to the context.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Expr {
     Var(String),
